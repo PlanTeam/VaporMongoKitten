c2be5e25bc5bc42b8737f808a14eb04df8e5285b
Joannis Orlandos <j.orlandos@jakajima.eu>
2016-10-07 21:36:44 +0200

    Fixed an unused loc

--- Sources/VaporMongoKitten/InputValidation.swift
+++ Sources/VaporMongoKitten/InputValidation.swift
@@ -163,10 +163,6 @@ public func extract(from request: Request, requiredFields: [String : KeyType] = 
     
     let document = try Document(extendedJSON: json)
     
-    
-    request.data
-    
-    
     try validate(document: document, requiredFields: requiredFields, optionalFields: optionalFields)
     
     return document
